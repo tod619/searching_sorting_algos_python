9d8a0450aff8980d1fb28f5d0f76fa0a8ca0ab84
tod619 <trevorodwyer619@hotmail.com>
2023-08-01 20:39:30 +0100

    Create an optimized version of Bubble sort + test it in bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -11,7 +11,27 @@ def basic_bubble_sort(lst:list):
                 lst[j], lst[j+1] = lst[j+1], lst[j]
 
 
+# my_list = [12, 9, 17, 4, 1, 2, 8]
+# print(f"My List Before Sorting: {my_list}")
+# basic_bubble_sort(my_list)
+# print(f"MY List After Sorting: {my_list}")
+
+# Optimized Bubble Sort Using swapped flag
+def optimized_bubble_sort(lst:list):
+    n = len(lst)
+
+    for i in range(n):
+        # Set swapped Flag to false
+        swapped = False
+        for j in range(0, n-i-1):
+            if lst[j] > lst[j + 1]:
+                lst[j],lst[j + 1] = lst[j + 1], lst[j]
+                swapped = True
+
+        if not swapped:
+            break
+
 my_list = [12, 9, 17, 4, 1, 2, 8]
 print(f"My List Before Sorting: {my_list}")
-basic_bubble_sort(my_list)
+optimized_bubble_sort(my_list)
 print(f"MY List After Sorting: {my_list}")
