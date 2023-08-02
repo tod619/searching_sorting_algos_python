a5914395f574a5dff937362fdb7e1df144a23cab
tod619 <trevorodwyer619@hotmail.com>
2023-08-02 21:45:51 +0100

    Answer an interview question with selection sort algo

--- selection_sort.py
+++ selection_sort.py
@@ -19,3 +19,28 @@ print(f"The unsorted list: {my_list}")
 selection_sort(my_list)
 
 print(f"The Sorted List: {my_list}")
+
+# Create an algoritm that compares the rankings of participants
+# The function will return the number of swaps + the first and last place participants
+def sort_participants(list):
+    num_swaps = 0
+    for i in range(len(list)):
+        min_index = i
+        
+        for curr_index in range(i+1, len(list)):
+            if list[min_index][1] > list[curr_index][1]:
+                min_index = curr_index
+                
+        list[i], list[min_index] = list[min_index], list[i]
+        
+        num_swaps += 1
+        
+    return(num_swaps, list[0][0], list[-1][0])
+            
+
+
+a = [("Anna", 4), ("Federico", 1), ("Mandy", 3), ("Igor", 2)]
+
+b = sort_participants(a)
+
+print(b)
