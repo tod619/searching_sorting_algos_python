37df5b294b1abf01bd0d966f1cef157202ca9ade
tod619 <trevorodwyer619@hotmail.com>
2023-08-15 21:16:15 +0100

    Create merge sort function in merge_sort.py file

--- merge_sort.py
+++ merge_sort.py
@@ -1,3 +1,16 @@
 # Implementation of the merge sort algorithm
 # 15/08/2023
 
+def merge_sort(lst:list):
+    if len(lst) == 0 or len(lst) == 1:
+        return lst
+    else:
+        middle_index = len(lst) // 2
+
+    left = merge_sort(lst[:middle_index])
+    right = merge_sort(lst[middle_index:])
+
+    return merge(left, right)
+
+def merge():
+    pass
