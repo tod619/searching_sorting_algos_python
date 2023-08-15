993715646f58fc1ae1bba67b6d5ed03e930c7e19
tod619 <trevorodwyer619@hotmail.com>
2023-08-15 21:37:21 +0100

    Create merge function in merge_sort.py file

--- merge_sort.py
+++ merge_sort.py
@@ -12,5 +12,25 @@ def merge_sort(lst:list):
 
         return merge(left, right)
 
-def merge():
-    pass
+def merge(left_half:list, right_half:list):
+    if not left_half or not right_half:
+        return left_half or right_half
+    
+    result = []
+
+    i, j = 0, 0
+
+    while True:
+
+        if left_half[i] < right_half[j]:
+            result.append(left_half[i])
+            i += 1
+        else:
+            result.append(right_half[j])
+            j += 1
+
+        if i == len(left_half) or j == len(right_half):
+            result.extend(left_half[i:] or right_half[j:])
+            break
+
+    return result
