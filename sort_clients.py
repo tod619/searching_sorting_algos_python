58d4852bf725e8d053bbb9e12d433c5172f3fe74
tod619 <trevorodwyer619@hotmail.com>
2023-08-16 21:41:16 +0100

    Create sort clients.py file

---
+++ sort_clients.py
@@ -0,0 +1,5 @@
+# Sort clients for an airline by miles traveled using merge sort algorithm
+# A cleint is made of a tuple which contains (<Name>, <Age>, <Miles Travelled>)
+# All the client tuples are stored in a list
+# Sort the list so that the clients with the fewest miles appear first
+# 16/08/2023
