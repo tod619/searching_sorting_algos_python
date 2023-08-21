4d0e999055a433d1d923996c2fc69eae6955eec5
tod619 <trevorodwyer619@hotmail.com>
2023-08-21 21:23:41 +0100

    Create partition function + test algo in quick_sort.py file

--- quick_sort.py
+++ quick_sort.py
@@ -8,5 +8,22 @@ def quicksort(lst, low, high):
         quicksort(lst, low, pivot_index - 1)
         quicksort(lst, pivot_index + 1, high)
 
-def partition():
-    pass
+def partition(lst, low, high):
+    pivot = lst[high]
+
+    i = low - 1
+
+    for j in range(low, high):
+        if lst[j] <= pivot:
+            i += 1
+            lst[i], lst[j] = lst[j], lst[i]
+
+    lst[i+1], lst[high] = lst[high], lst[i+1]
+
+    return i + 1
+
+
+a = [4, 9, 5, 3, 1, 2]
+print(f"Unsorted List: {a}")
+quicksort(a, 0, len(a) - 1)
+print(f"Sorted List: {a}")
