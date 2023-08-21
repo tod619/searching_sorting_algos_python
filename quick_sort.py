352876dc06167e7af13d99f0c694f17d0dcd1f93
tod619 <trevorodwyer619@hotmail.com>
2023-08-21 20:54:28 +0100

    Create quicksort function in quick_sort.py file

--- quick_sort.py
+++ quick_sort.py
@@ -1,2 +1,12 @@
 # An implementation fo the quick sort algorithm
 # 17/08/2023
+
+def quicksort(lst, low, high):
+    if low < high:
+        pivot_index = partition(lst, low, high)
+
+        quicksort(lst, low, pivot_index - 1)
+        quicksort(lst, pivot_index + 1, high)
+
+def partition():
+    pass
