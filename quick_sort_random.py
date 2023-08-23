540644459cbaded8ff547555a1a59cd41c3f8ed9
tod619 <trevorodwyer619@hotmail.com>
2023-08-23 21:49:36 +0100

    Create quicksort algo using random pivot in quick_sort_random.py file

--- quick_sort_random.py
+++ quick_sort_random.py
@@ -1,3 +1,46 @@
-# An implementation of the quicksort algorithm selcting a random piviot
+# An implementation of the quicksort algorithm selcting a random pivot for the partition
 # 23/08/2023
 
+import random
+
+def quicksort(lst, low, high):
+    # If the interval is valid
+    if low < high:
+        # partition the list and get the partioning index
+        pivot_index = random_pivot_partition(lst, low, high)
+
+        # Sort the elements of the list
+        # that come before and after the partition
+        quicksort(lst, low, pivot_index - 1)
+        quicksort(lst, pivot_index + 1, high)
+
+def random_pivot_partition(lst, low, high):
+    # generate a random index by selecting an element randomly
+    random_index = random.randrange(low, high)
+
+    # Swap the random element with the last element in the list
+    # we use the last element in the list as are pivot re: Lomuto's pivot scheme
+    lst[high], lst[random_index] = lst[random_index], lst[high]
+
+    return partition(lst, low, high)
+
+def partition(lst, low, high):
+    # Select pivot based on Lomuto's pivot scheme
+    pivot = lst[high]
+
+    i = low - 1
+
+    for j in range(low, high):
+        # if the current element is smaller or equal to the pivot
+        if lst[j] <= pivot:
+            i += 1
+            lst[i], lst[j] = lst[j], lst[i]
+
+    lst[i + 1], lst[high] = lst[high], lst[i+1]
+    return i + 1
+
+
+a = [4, 9, 5, 3, 1, 2]
+print(f"Unsorted List: {a}")
+quicksort(a, 0, len(a) - 1)
+print(f"Sorted List: {a}")
