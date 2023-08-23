4e279154b9627bd50f9e633de419fc10d847b103
tod619 <trevorodwyer619@hotmail.com>
2023-08-23 22:26:54 +0100

    Create the quicsort_question.py file

---
+++ quicksort_question.py
@@ -0,0 +1,4 @@
+# A school wants to reward its high performing instructors
+# It has asked its students to rate each instructor between 0 - 5
+# Any instructor that scores a 4.5 or higher the school wants to reward
+# the school would like to know how many instructors acheive the higher scores
